ad7b2e4e2f2fbaf7035e561b8f3c4a70d53c064c
Jakub Fronczyk <jakubfronczyk@gmail.com>
2022-09-16 16:31:37 +0200

    Added first components

--- app.js
+++ app.js
@@ -1,24 +1,36 @@
-function PageComponent (){
-    return(<div>
-        <header>
-            <nav>
-                <img src="images/react-logo.png" width="50px"/>
-            </nav>
-        </header>
-        <h1>Fun facts about React</h1>
-        <ol>
-            <li>Was first released in 2013</li>
-            <li>Was orginally created by Jordan Walke</li>
-            <li>Has well over 100K stars on GitHub</li>
-            <li>Is maintained by Facebook</li>
-            <li>Powers thousands of enterprise app, including mobile apps</li>
-        </ol>
-        <footer>
+import Header from 'Header.js';
+
+function Footer() {
+    return(
+        <footer className="footer">
             <small>© 2022 Jakub Fronczyk development. All rights reserved.</small>
         </footer>
+    )
+}
+
+function MainContent() {
+    return(
+        <div className="main-content">
+            <h1>Fun facts about React</h1>
+            <ol>
+                <li>Was first released in 2013</li>
+                <li>Was orginally created by Jordan Walke</li>
+                <li>Has well over 100K stars on GitHub</li>
+                <li>Is maintained by Facebook</li>
+                <li>Powers thousands of enterprise app, including mobile apps</li>
+            </ol>
+        </div>
+    )
+}
+function Page (){
+    return(
+    <div className="page">
+        <Header />
+        <MainContent />
+        <Footer />
     </div>
     
     )
 }
 
-ReactDOM.render (PageComponent(), document.getElementById('root'))
+ReactDOM.render (<Page/>, document.getElementById('root'))
